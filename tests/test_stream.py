2b7905361d6063cd809790e237e604ed9992273a
drkitty <bumblingkitty@gmail.com>
2014-10-25 20:35:00 -0700

    Add tests

---
+++ tests/test_stream.py
@@ -0,0 +1,69 @@
+from unittest import TestCase
+
+from stream import End, Invalid, Stream
+
+
+class StreamTests(TestCase):
+    def test_get(self):
+        s = Stream('0123')
+
+        self.assertEqual(s.get(), '0')
+        self.assertEqual(s.get(), '1')
+        self.assertEqual(s.get(), '2')
+        self.assertEqual(s.get(), '3')
+        self.assertEqual(s.get(), None)
+
+    def test_context_manager_valid(self):
+        s = Stream('012345')
+
+        with s as ss:
+            with ss as sss:
+                self.assertEqual(sss.get(), '0')
+                self.assertEqual(sss.get(), '1')
+            self.assertEqual(sss.get(), '2')
+            self.assertEqual(sss.get(), '3')
+        self.assertEqual(sss.get(), '4')
+        self.assertEqual(sss.get(), '5')
+        self.assertEqual(sss.get(), None)
+
+    def test_context_manager_invalid(self):
+        s = Stream('012345')
+
+        self.assertEqual(s.get(), '0')
+        self.assertEqual(s.get(), '1')
+        with self.assertRaises(Invalid):
+            with s as ss:
+                self.assertEqual(ss.get(), '2')
+                self.assertEqual(ss.get(), '3')
+                with self.assertRaises(Invalid):
+                    with ss as sss:
+                        self.assertEqual(sss.get(), '4')
+                        self.assertEqual(sss.get(), '5')
+                        raise Invalid()
+                with self.assertRaises(Invalid):
+                    with ss as sss:
+                        self.assertEqual(sss.get(), '4')
+                        self.assertEqual(sss.get(), '5')
+                        raise Invalid()
+                self.assertEqual(ss.get(), '4')
+                self.assertEqual(ss.get(), '5')
+                raise Invalid()
+        with self.assertRaises(Invalid):
+            with s as ss:
+                self.assertEqual(ss.get(), '2')
+                self.assertEqual(ss.get(), '3')
+                with self.assertRaises(Invalid):
+                    with ss as sss:
+                        self.assertEqual(sss.get(), '4')
+                        self.assertEqual(sss.get(), '5')
+                        raise Invalid()
+                with self.assertRaises(Invalid):
+                    with ss as sss:
+                        self.assertEqual(sss.get(), '4')
+                        self.assertEqual(sss.get(), '5')
+                        raise Invalid()
+                self.assertEqual(ss.get(), '4')
+                self.assertEqual(ss.get(), '5')
+                raise Invalid()
+        self.assertEqual(s.get(), '2')
+        self.assertEqual(s.get(), '3')
